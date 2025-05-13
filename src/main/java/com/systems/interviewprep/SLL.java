c3e98a29e3c0cb15188c8005b015e2b65bc49aa6
Bassell Dari Iddisah <bbasssell16@gmail.com>
2025-05-13 04:25:34 +0000

    Representing SLL

---
+++ src/main/java/com/systems/interviewprep/SLL.java
@@ -0,0 +1,10 @@
+package com.systems.interviewprep;
+
+public class SLL {
+    private ListNode head;
+
+    private class ListNode {
+        private int data;
+        private ListNode next;
+    }
+}
